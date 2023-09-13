2984da9bde4ab3da7fa3cc8269cc49c8cf07d2d9
MJP1998 <mpilcer@hotmail.fr>
2023-09-13 17:34:22 +0200

    minor edit to upload directly

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from pydub import AudioSegment
 
 from src.csv_reader import CSVReader
 from src.text_to_speech import TextToSpeech
+from src.uploader import Uploader
 from src.utils import Config, Audio
 from src.video_generator import VideoGeneration
 
@@ -11,7 +12,7 @@ from src.video_generator import VideoGeneration
 def main():
     # Read CSV
     config = Config()
-    csv_name = "ghibli"
+    csv_name = "SkyColors"
     csv_reader = CSVReader(f"csv/{csv_name}.csv")
     tts = TextToSpeech()
     videos = csv_reader.get_video_entries()
@@ -27,12 +28,19 @@ def main():
         # Generate audio from script
         audio_object = tts.get_audio(video.script) if not video.script.isspace() and video.script else Audio(audio_segment=AudioSegment.silent(duration=40000))
         music_file_path = os.path.join(config.music_dir, f"1.mp3")
-        music_object = None # Audio(music_file_path)
+        music_object = Audio(music_file_path)
         video_file_path = video_generator.generate_video(audio_object, video.script, video.title,
                                                          video.filename or csv_name, music_object)
 
         # (Optional) Upload video to a platform
-        # upload_video(video_path)
+        # uploader = Uploader(video)
+        # schedule = True
+        # schedule_day = "17"
+        # schedule_time = "00:30"
+        # # uploader.upload_to_youtube(schedule, schedule_day, schedule_time)
+        # # uploader.upload_to_tiktok(schedule, schedule_day, schedule_time)
+        # # uploader.upload_to_instagram(schedule, schedule_day, schedule_time)
+        # uploader.upload_to_all(schedule, schedule_day, schedule_time)
 
 
 if __name__ == "__main__":
